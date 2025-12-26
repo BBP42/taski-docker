aa0d690a6672850eeef88d58157e07d100796d41
Bagrat Zarandia <zarandiabagrat3@gmail.com>
2025-12-26 14:50:38 -0800

    vvv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.161.205', '127.0.0.1', 'localhost', 'skx.zapto.org'] 
+ALLOWED_HOSTS = ['84.201.161.205', '127.0.0.1', 'localhost', 'skx.zapto.org']
 
 
 # Application definition
@@ -72,7 +72,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 
 # Password validation
