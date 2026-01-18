455ab7446b7fc6ff66085abb38874fbe2c2feed8
Bagrat Zarandia <zarandiabagrat3@gmail.com>
2026-01-17 21:13:47 -0800

    BBP42

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,12 +6,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.161.205', '127.0.0.1', 'localhost', 'skx.zapto.org']
+ALLOWED_HOSTS = ['84.201.161.205', 'localhost', 'skx.zapto.org', '127.0.0.1']
 
 
-# Application definition
+# Application
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
@@ -62,8 +62,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # Меняем настройку Django: теперь для работы будет использоваться
-        # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
@@ -111,7 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 STATIC_ROOT = BASE_DIR / 'collected_static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
